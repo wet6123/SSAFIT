1ef7d70f8b56827eeb482b574077fbc5d5350e2e
신은정 <ejshin0213@naver.com>
2022-05-19 11:29:53 +0900

    add FooterNav

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div>
     <header-nav></header-nav>
     <router-view />
+    <footer-nav></footer-nav>
   </div>
 </template>
 
 <script>
 import HeaderNav from "@/components/common/HeaderNav.vue";
+import FooterNav from "@/components/common/FooterNav.vue";
 export default {
   components: {
     HeaderNav,
+    FooterNav,
   },
 };
 </script>
