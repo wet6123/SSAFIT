d31fc0e4f78014e8382a839efd5fb4f056b48b38
JunSeong <gentlmanjun@naver.com>
2022-05-21 21:26:45 +0900

    refector infinity scroll to use vuex action

--- src/components/common/VideoInf.vue
+++ src/components/common/VideoInf.vue
@@ -19,8 +19,10 @@
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 import lodash from "lodash";
+import { mapState } from "vuex";
+
 export default {
   name: "VideoInf",
   data() {
@@ -28,21 +30,24 @@ export default {
       photos: [], //서버 통신으로 받아오는 비디오 배열
       shownPhotos: [], //보여주는 비디오 배열
       current: 0, //지금 보여주는 비디오 개수
-      loaded: 0, //지금까지 로딩된 비디오 배열의 비디오 수
+      loaded: 0, //지금까지 로딩된 비디오 배열의 비디오 수 => 이거를 howMany씩 잘라서 shownPhotos에 넣는다
       howMany: 15, //한번에 몇개 영상 갱신?
     };
   },
+  computed: {
+    ...mapState(["videos"]),
+  },
   methods: {
     getPhotos: function () {
-      axios.get("http://localhost:9999/video/all").then((res) => {
+      this.$store.dispatch("getVideos").then(() => {
         //title 특문 디코딩
-        for (let video of res.data) {
+        for (let video of this.videos) {
           console.log("load videos");
           video.title = lodash.unescape(video.title);
         }
         //data
-        this.photos = [...this.photos, ...res.data];
-        this.loaded += res.data.length;
+        this.photos = [...this.photos, ...this.videos];
+        this.loaded += this.videos.length;
         console.log(this.photos.length);
         console.log("loaded: " + this.loaded + " current: " + this.current);
         this.addFromLoaded();
