84c1930c422483a6f4b13f329da1b003cb56608c
JunSeong <gentlmanjun@naver.com>
2022-05-21 18:51:38 +0900

    add getVideos action

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import router from '@/router'
-import axios from 'axios'
+import Vue from "vue";
+import Vuex from "vuex";
+import router from "@/router";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const REST_API = `http://localhost:9999`
+const REST_API = `http://localhost:9999`;
 
 export default new Vuex.Store({
   state: {
@@ -18,134 +18,170 @@ export default new Vuex.Store({
     review: {},
 
     // 로그인 관련
-    isLogin: false
+    isLogin: false,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
     // 비디오 관련
+    GET_VIDEOS(state, payload) {
+      state.videos = payload;
+    },
     GET_VIDEO(state, payload) {
-      state.video = payload
+      state.video = payload;
     },
 
     // 리뷰 관련
     GET_REVIEWS(state, payload) {
-      state.reviews = payload
+      state.reviews = payload;
     },
     GET_REVIEW(state, payload) {
-      state.review = payload 
+      state.review = payload;
     },
     CREATE_REVIEW(state, payload) {
-      state.reviews.push(payload)
+      state.reviews.push(payload);
     },
     UPDATE_REVIEW(state, payload) {
-      state.review = payload
+      state.review = payload;
     },
 
     // 로그인 관련
     USER_LOGIN(state) {
-      state.isLogin = true
-    }
-    
+      state.isLogin = true;
+    },
   },
   actions: {
-    getVideo({ commit }, id) {
-      const API_URL = `${REST_API}/video/detail/${id}`
-      axios({
-        url: API_URL,
-        method: 'GET',
-      }).then(res => {
-        commit('GET_VIDEO', res.data)
-      }).catch((err) => {
-        console.log(err)
-      })
-    },
-    getReviews({ commit }, payload) {
-      let params = null
+    //비디오 관련
+    getVideos({ commit }, payload) {
+      let params = null;
 
       if (payload) {
-       params = payload 
+        params = payload;
       }
-      const API_URL = `${REST_API}/board` // 백엔드 참고
+      const API_URL = `${REST_API}/video/all`; // 백엔드 참고
       axios({
         url: API_URL,
-        method: 'GET',
-        params
-      }).then((res) => {
-        console.log(res)
-        commit('GET_REVIEWS', res.data)
-        
-      }).catch((err) => {
-        console.log(err)
+        method: "GET",
+        params,
       })
+        .then((res) => {
+          console.log(res);
+          commit("GET_VIDEOS", res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getVideo({ commit }, id) {
+      const API_URL = `${REST_API}/video/detail/${id}`;
+      axios({
+        url: API_URL,
+        method: "GET",
+      })
+        .then((res) => {
+          commit("GET_VIDEO", res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    //리뷰 관련
+    getReviews({ commit }, payload) {
+      let params = null;
+
+      if (payload) {
+        params = payload;
+      }
+      const API_URL = `${REST_API}/board`; // 백엔드 참고
+      axios({
+        url: API_URL,
+        method: "GET",
+        params,
+      })
+        .then((res) => {
+          console.log(res);
+          commit("GET_REVIEWS", res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     getReview({ commit }, id) {
-      const API_URL = `${REST_API}/board/${id}` // 백엔드 참고
+      const API_URL = `${REST_API}/board/${id}`; // 백엔드 참고
       axios({
         url: API_URL,
-        method: 'GET',
-      }).then(res => {
-        commit('GET_REVIEW', res.data)
-      }).catch((err) => {
-        console.log(err)
+        method: "GET",
       })
+        .then((res) => {
+          commit("GET_REVIEW", res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     createReview({ commit }, review) {
-      const API_URL = `${REST_API}/board` // 백엔드 참고
+      const API_URL = `${REST_API}/board`; // 백엔드 참고
       axios({
         url: API_URL,
-        method: 'POST',
-        params: review
-      }).then(() => {
-        commit('CREATE_REVIEW', review)
-        router.push("/board")
-      }).catch((err) => {
-        console.log(err)
+        method: "POST",
+        params: review,
       })
+        .then(() => {
+          commit("CREATE_REVIEW", review);
+          router.push("/board");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     updateReview({ commit }, review) {
-      const API_URL = `${REST_API}/board` // 백엔드 참고
+      const API_URL = `${REST_API}/board`; // 백엔드 참고
       axios({
         url: API_URL,
-        method: 'PUT',
-        params: review
-      }).then(() => {
-        commit('UPDATE_REVIEW', review)
-        router.push({ name: 'vdetail', params: { id: review.id } })
-      }).catch((err) => {
-        console.log(err)
+        method: "PUT",
+        params: review,
       })
+        .then(() => {
+          commit("UPDATE_REVIEW", review);
+          router.push({ name: "vdetail", params: { id: review.id } });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
     deleteReview(context, id) {
-      context
-      const API_URL = `${REST_API}/board/${id}` // 백엔드 참고
+      context;
+      const API_URL = `${REST_API}/board/${id}`; // 백엔드 참고
       axios({
         url: API_URL,
-        method: 'DELETE',
-      }).then(() => {
-        router.push({name: 'vdetail'})
-      }).catch((err) => {
-        console.log(err)
+        method: "DELETE",
       })
+        .then(() => {
+          router.push({ name: "vdetail" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
 
     // 로그인 관련
     userLogin({ commit }, user) {
-      const API_URL = `${REST_API}/user/login`
+      const API_URL = `${REST_API}/user/login`;
       axios({
         url: API_URL,
-        method: 'POST',
-        params: user
-      }).then(res => {
-        console.log(res)
-        commit('USER_LOGIN')
-        localStorage.setItem("access-token", res.data["access-token"])
-        router.push({name: 'main'})
-      }).catch((err) => {
-        console.log(err)
+        method: "POST",
+        params: user,
       })
-    }
+        .then((res) => {
+          console.log(res);
+          commit("USER_LOGIN");
+          localStorage.setItem("access-token", res.data["access-token"]);
+          router.push({ name: "main" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
