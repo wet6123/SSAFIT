6a4841a6dad7674b135226b09aad0d2d98df0760
신은정 <ejshin0213@naver.com>
2022-05-24 15:01:44 +0900

    edit review rate

--- src/components/vdetail/review/VdetailReviewList.vue
+++ src/components/vdetail/review/VdetailReviewList.vue
@@ -2,61 +2,69 @@
   <div class="container">
     <div v-if="reviews.length">
       <b-table-simple hover responsive>
-        <b-tr v-for="(review, index) in reviews" :key="index+'p'">
-          <b-td>
-            (프로필)
-          </b-td>
+        <b-tr v-for="(review, index) in reviews" :key="index + 'p'">
+          <b-td> (프로필) </b-td>
           <b-td>
             <b-row>
               <b-col>{{ review.writer }}</b-col>
-              <b-col>{{ review.rate }}</b-col>
+              <b-col
+                ><star-rating
+                  :rating="review.rate"
+                  :read-only="true"
+                  :increment="0.5"
+                  :star-size="20"
+                ></star-rating
+              ></b-col>
             </b-row>
             <b-tr>
               {{ review.content }}
             </b-tr>
-            <b-tr class="review" :id="index+'p'" style="display: none">
+            <b-tr class="review" :id="index + 'p'" style="display: none">
               <br />
               <b-row>
-              <b-col cols="1">
-                <v-icon>mdi-account-circle</v-icon>
-              </b-col>
-              <b-col class="review-write" cols="7">
-                <v-text-field
-                  v-model="content"
-                  label="답글을 작성해주세요"
-                ></v-text-field>
-              </b-col>
-              <b-col class="btn" cols="4">
-                <b-button variant="outline-secondary" @click="createReply(review.id)" class="btn-sm"
-                  >등록</b-button
-                >
-                <b-button
-                  class="btn-sm"
-                  variant="outline-secondary"
-                  @click="toggleCreateReply(index)"
-                  >취소</b-button
-                >
-              </b-col>
+                <b-col cols="1">
+                  <v-icon>mdi-account-circle</v-icon>
+                </b-col>
+                <b-col class="review-write" cols="7">
+                  <v-text-field
+                    v-model="content"
+                    label="답글을 작성해주세요"
+                  ></v-text-field>
+                </b-col>
+                <b-col class="btn" cols="4">
+                  <b-button
+                    variant="outline-secondary"
+                    @click="createReply(review.id)"
+                    class="btn-sm"
+                    >등록</b-button
+                  >
+                  <b-button
+                    class="btn-sm"
+                    variant="outline-secondary"
+                    @click="toggleCreateReply(index)"
+                    >취소</b-button
+                  >
+                </b-col>
               </b-row>
             </b-tr>
-            <br/>
+            <br />
             <b-tr>
-            <b-link @click="toggleShowReply(review.id)">답글 {{replyNum[review.id]}}개</b-link>
-            <div class="reply" :id="review.id" style="display: none">
-              <div v-if="replyNum[review.id] == 0">
-                등록된 답글이 없습니다.
-              </div>
-              <b-tr v-for="(reply, index) in replies" :key="index+'c'">
-                <div v-if="review.id === reply.re_id">
-                  <b-td>
-                    (프로필)
-                  </b-td>
-                  <b-td>
-                    <b-tr>{{ reply.writer }}</b-tr>
-                    <b-tr>{{ reply.content }}</b-tr>
-                  </b-td>
-                  <b-td>
-                    <div v-if="userinfo.id === reply.uid">
+              <b-link @click="toggleShowReply(review.id)"
+                >답글 {{ replyNum[review.id] }}개</b-link
+              >
+              <div class="reply" :id="review.id" style="display: none">
+                <div v-if="replyNum[review.id] == 0">
+                  등록된 답글이 없습니다.
+                </div>
+                <b-tr v-for="(reply, index) in replies" :key="index + 'c'">
+                  <div v-if="review.id === reply.re_id">
+                    <b-td> (프로필) </b-td>
+                    <b-td>
+                      <b-tr>{{ reply.writer }}</b-tr>
+                      <b-tr>{{ reply.content }}</b-tr>
+                    </b-td>
+                    <b-td>
+                      <div v-if="userinfo.id === reply.uid">
                         <v-btn small class="mx-2">
                           <v-icon dark> mdi-pencil </v-icon>
                         </v-btn>
@@ -64,17 +72,17 @@
                           <v-icon dark> mdi-delete </v-icon>
                         </v-btn>
                       </div>
-                  </b-td>
-                </div>
-              </b-tr>
-            </div>
-          </b-tr>
+                    </b-td>
+                  </div>
+                </b-tr>
+              </div>
+            </b-tr>
           </b-td>
           <b-td>
-              <v-btn @click="toggleCreateReply(index+'p')">답글</v-btn>
+            <v-btn @click="toggleCreateReply(index + 'p')">답글</v-btn>
           </b-td>
           <b-td>
-              <div v-if="userinfo.id === review.uid">
+            <div v-if="userinfo.id === review.uid">
               <v-btn class="mx-2">
                 <v-icon dark> mdi-pencil </v-icon>
               </v-btn>
@@ -92,7 +100,7 @@
 
 <script>
 import { mapState } from "vuex";
-
+import StarRating from "vue-star-rating";
 export default {
   name: "VdetailReviewList",
   data() {
@@ -102,7 +110,10 @@ export default {
     };
   },
   computed: {
-    ...mapState(["video","reviews", "userinfo", "replies", "replyNum"]),
+    ...mapState(["video", "reviews", "userinfo", "replies", "replyNum"]),
+  },
+  components: {
+    StarRating,
   },
   methods: {
     toggleCreateReply(index) {
@@ -125,7 +136,6 @@ export default {
       this.$store.dispatch("createReview", newReply);
       this.content = "";
     },
-
 
     // getReview() {},
     // deleteReview() {
