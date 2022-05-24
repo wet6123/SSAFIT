43f6ef0141baad6bb90f776e717c023d2d971546
JunSeong <gentlmanjun@naver.com>
2022-05-24 14:58:00 +0900

    add css at main buttons

--- src/components/main/MainVideo.vue
+++ src/components/main/MainVideo.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-    <div>
-      <button @click="part = ''" style="border: solid; margin: 5px">
-        전체
-      </button>
+    <div class="btnContainer">
+      <button @click="part = ''" class="partBtn" id="totalBtn">전체</button>
       <button
         v-for="(partName, idx) in parts"
         v-bind:key="idx"
         @click="part = partName"
-        style="border: solid; margin: 5px"
+        class="partBtn"
       >
         {{ partName }}
       </button>
@@ -43,4 +41,38 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#totalBtn {
+  color: #f82f62;
+}
+
+.partBtn {
+  margin: 10px 10px;
+  position: relative;
+  border: none;
+  display: inline-block;
+  min-width: 110px;
+  padding: 10px 30px;
+  border-radius: 15px;
+  font-family: "paybooc-Light", sans-serif;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+  text-decoration: none;
+  font-weight: 600;
+  transition: 0.25s;
+
+  background-color: rgb(255, 255, 255);
+  color: #1e6b7b;
+}
+.partBtn:hover {
+  letter-spacing: 2px;
+  transform: scale(1.2);
+  cursor: pointer;
+}
+.partBtn:active {
+  transform: scale(1.5);
+}
+
+.btnContainer {
+  margin: 20px;
+}
+</style>
